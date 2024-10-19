93eea9b655d5f04c2413df0bd85b9f4dee28f293
merve türk <mtturk26@gmail.com>
2024-10-19 20:09:57 +0200

    first commit

---
+++ src/main/java/com/robotdreams/JavaSpringEduClass/JavaSpringEduClassApplication.java
@@ -0,0 +1,13 @@
+package com.robotdreams.JavaSpringEduClass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringEduClassApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringEduClassApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/robotdreams/JavaSpringEduClass/JavaSpringEduClassApplicationTests.java
@@ -0,0 +1,13 @@
+package com.robotdreams.JavaSpringEduClass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringEduClassApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
